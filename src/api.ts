7368dcf7caab432675b8e9faa970d4ff4620fe9e
David Meadows <dtmeadows@stainlessapi.com>
2024-11-13 11:19:07 -0500

    extract things to better functions

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,73 @@
+import {Feature, GeoJsonProperties, Geometry} from 'geojson';
 const apiBaseUrl = import.meta.env.VITE_BASE_URL;
+
+export type Lifer = {
+  common_name: string;
+  latitude: number;
+  longitude: number;
+  date: string;
+  taxonomic_order: number;
+  location: string;
+  location_id: string;
+  species_code: string;
+};
+
+type Location = {
+  location_name: string;
+  latitude: number;
+  longitude: number;
+  location_id: string;
+};
+
+type LocationToLifers = {
+  location: Location;
+  lifers: Lifer[];
+};
+
+type LocationByLiferResponse = {
+  [key: string]: LocationToLifers;
+};
+
+export function lifersToGeoJson(response: LocationByLiferResponse) {
+  return Object.values(response).map((l) => {
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [l.location.longitude, l.location.latitude],
+      },
+      properties: {
+        title: l.location.location_name,
+        lifers: l.lifers,
+        liferCount: l.lifers.length,
+        speciesCodes: l.lifers.map((lifer) => lifer.species_code).join(","),
+      },
+    } as Feature<Geometry, GeoJsonProperties>;
+  });
+}
+
+export function nearbyObservationsToGeoJson(
+  lifers: LocationByLiferResponse,
+): Feature<Geometry, GeoJsonProperties>[] {
+  if (!lifers) return [];
+  return Object.values(lifers).flatMap((entry) => {
+    const feature = {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [entry.location.longitude, entry.location.latitude],
+      },
+      properties: {
+        title: entry.location.location_name,
+        lifers: entry.lifers,
+        liferCount: entry.lifers.length,
+        speciesCodes: entry.lifers.map((lifer) => lifer.species_code).join(","),
+      },
+    };
+
+    return feature as Feature<Geometry, GeoJsonProperties>;
+  });
+}
 
 // todo: dedupe all of this
 
