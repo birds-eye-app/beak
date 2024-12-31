7270c0887d701c2d0db0e05dcd680dc3c02683d7
David Meadows <dtmeadows@stainlessapi.com>
2024-12-30 19:37:12 -0500

    inital things working

--- src/chirped/Chirped.tsx
+++ src/chirped/Chirped.tsx
@@ -1,20 +1,97 @@
-import { WaitAndUploadModal } from "../WaitAndUploadModal";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { ChirpedContext, ChirpedContextType } from "./Context";
+import { UploadCSV } from "./Upload";
+import { parseObservations } from "./parse";
+import { performChirpedCalculations } from "./calculate";
+import { makeNewChirpedContext } from "./helpers";
+
+const TestComponent = () => {
+  const chirped = useContext(ChirpedContext);
+  // make a simple html debug table of things like:
+  // allObservations.length, yearObservations.length, lifers.length
+  return (
+    <>
+      <table>
+        <tbody>
+          <tr>
+            <td>All Observations</td>
+            <td>{chirped.allObservations.length}</td>
+            <td>{chirped.allObservations[0]?.scientificName}</td>
+          </tr>
+          <tr>
+            <td>Year Observations</td>
+            <td>{chirped.yearObservations.length}</td>
+            <td>{chirped.yearObservations[0]?.scientificName}</td>
+          </tr>
+          <tr>
+            <td>Lifers</td>
+            <td>{chirped.lifeList.length}</td>
+            <td>{chirped.lifeList[0]?.scientificName}</td>
+          </tr>
+          {/* print each stat with the key as the first cell and the value as the second */}
+          {Object.entries(chirped.yearStats).map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}</td>
+              <td>{value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+const CurrentYear = 2024;
+
 export function Chirped() {
+  const [fileContents, setFileContents] = useState("");
+  const [chirpedObservations, setChirpedObservations] =
+    useState<ChirpedContextType>(makeNewChirpedContext());
   const [showUploadModal, setShowUploadModal] = useState(true);
+
+  const onUploadComplete = async (contents: string) => {
+    setShowUploadModal(false);
+    setFileContents(contents);
+  };
+
+  useEffect(() => {
+    async function runObs() {
+      try {
+        const observations = await parseObservations(fileContents);
+        console.debug("found observations", observations.length);
+        const chirped = await performChirpedCalculations(
+          observations,
+          CurrentYear,
+        );
+        setChirpedObservations(chirped);
+      } catch (e) {
+        alert(`Error parsing observations: ${JSON.stringify(e)}`);
+      }
+    }
+
+    runObs();
+  }, [fileContents]);
+
+  console.debug(chirpedObservations.allObservations.length);
+
+  if (chirpedObservations.allObservations.length === 0) {
+    return (
+      <div>
+        <h1>Chirped</h1>
+        <p>Welcome!</p>
+        <UploadCSV
+          showModal={showUploadModal}
+          onUploadComplete={onUploadComplete}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
-      <h1>Chirped</h1>
-      <p>Welcome!</p>
-      <WaitAndUploadModal
-        showModal={showUploadModal}
-        onClose={() => {
-          setShowUploadModal(false);
-        }}
-        onUploadComplete={() => {}}
-        canClose={false}
-      />
+      <ChirpedContext.Provider value={chirpedObservations}>
+        <TestComponent />
+      </ChirpedContext.Provider>
     </div>
   );
 }
