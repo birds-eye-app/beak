14379f07ee956e069784b4e1bb40d97cad884dcd
David Meadows <dtmeadows@stainlessapi.com>
2025-01-01 20:56:05 -0500

    fix

---
+++ src/chirped/components/FadeWithInitialDelay.tsx
@@ -0,0 +1,20 @@
+import { Fade } from "@mui/material";
+
+export type FadeInWithInitialDelayProps = {
+  in?: boolean;
+  timeout?: number;
+  initialDelay: number;
+  children: React.ReactElement;
+};
+
+export const FadeInWithInitialDelay = (props: FadeInWithInitialDelayProps) => {
+  return (
+    <Fade
+      in={props.in !== undefined ? props.in : true}
+      timeout={props.timeout || 2000}
+      style={{ transitionDelay: `${props.initialDelay}ms` }}
+    >
+      {props.children}
+    </Fade>
+  );
+};
