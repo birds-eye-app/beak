336cf316b75b7aa1e95bf20a625639dc52d9f1ef
David Meadows <dtmeadows@stainlessapi.com>
2025-01-02 13:37:25 -0500

    add the thing

---
+++ src/chirped/contexts/UserSelections.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+import { QualitativeQuestionData } from "../components/slides/QualitativeInput";
+
+export type UserSelections = {
+  hotspotRanking: "checklists" | "timeSpent";
+  qualitativeQuestions: QualitativeQuestionData[];
+  setHotspotRanking: (ranking: "checklists" | "timeSpent") => void;
+  setQualitativeQuestions: (questions: QualitativeQuestionData[]) => void;
+};
+
+export const UserSelectionsContext = createContext<UserSelections>({
+  hotspotRanking: "checklists",
+  qualitativeQuestions: [],
+  setHotspotRanking: () => {},
+  setQualitativeQuestions: () => {},
+});
+
+import { ReactNode } from "react";
+
+export const UserSelections = ({ children }: { children: ReactNode }) => {
+  const [userSelections, setUserSelections] = useState<UserSelections>({
+    hotspotRanking: "checklists",
+    qualitativeQuestions: [],
+    setHotspotRanking: () => {},
+    setQualitativeQuestions: (questions: QualitativeQuestionData[]) => {
+      console.debug("qualitativeQuestions-set", questions);
+      setUserSelections((prevState) => ({
+        ...prevState,
+        qualitativeQuestions: questions,
+      }));
+    },
+  });
+
+  return (
+    <UserSelectionsContext.Provider value={userSelections}>
+      {children}
+    </UserSelectionsContext.Provider>
+  );
+};
